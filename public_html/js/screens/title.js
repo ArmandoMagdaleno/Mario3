22c402add14843563954d155bfbd85bc95b5a845
Armando Magdaleno <magdalenoarmando90@gmail.com>
2014-12-01 11:49:49 -0800

    new repository

---
+++ public_html/js/screens/title.js
@@ -0,0 +1,17 @@
+game.TitleScreen = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+                var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
+                me.game.world.addChild(titleImage, 1);
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+		; // TODO
+	}
+});
