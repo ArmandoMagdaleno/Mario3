bbdf9e7f643702b0da60a6d0fb7e774e56a9efce
Armando Magdaleno <magdalenoarmando90@gmail.com>
2014-12-01 12:15:59 -0800

    title

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -1,17 +1,36 @@
 game.TitleScreen = me.ScreenObject.extend({
-	/**	
-	 *  action to perform on state change
-	 */
-	onResetEvent: function() {	
-                var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
-                me.game.world.addChild(titleImage, 1);
-	},
-	
-	
-	/**	
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
-		; // TODO
-	}
+    /**	
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
+        me.game.world.addChild(titleImage, 1);
+        me.input.bindKey(me.input.KEY.ENTER, "START");
+        
+        
+        me.game.world.addChild(new (me.Renderable.extend ({
+            init: function(){
+                this._super(me.Renderable, 'init', [150, 30, me.game.viewport.width, me.game.viewport.height]);
+                this.font = new me.Font("arial", 46, "red");
+            },
+            
+            draw: function(renderer){
+                this.font.draw(renderer.getContext(), "Marioish", 450, 130);
+                this.font.draw(renderer.getContext(), "press ENTER to play", 250, 530);
+            }            
+        })));
+
+
+        this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
+            if (action === "start") {
+                me.state.change(me.state.PLAY);
+            }
+        });
+   },
+    /**	
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        me.input.unbindKey(me.input.KEY.ENTER);
+    }
 });
