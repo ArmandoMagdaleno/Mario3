03c6befd1042f379325ae0400a8dba8bf4ad3eeb
Armando Magdaleno <magdalenoarmando90@gmail.com>
2014-12-04 11:26:10 -0800

    .

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,7 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
 
                 me.levelDirector.loadLevel("ArmandoLevel01");
                 
-                this.resetPlayer(0, 400);
+                this.resetPlayer(0, 40);
                 
                 me.input.bindKey(me.input.KEY.RIGHT, "right");
 
